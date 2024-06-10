df0d2da253b7b9d223dabf25ecda6d41b8150c20
Agustín Schulze <agsp3rs3o@gmail.com>
2024-06-10 13:33:31 -0300

    Configurating product microservice

---
+++ services/product/src/main/java/com/ags/ecommerce/ProductApplication.java
@@ -0,0 +1,13 @@
+package com.ags.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApplication.class, args);
+	}
+
+}
