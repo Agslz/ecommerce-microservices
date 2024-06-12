c2056b14b897c562252b72db4d375b48b48bd17b
Agustín Schulze <agsp3rs3o@gmail.com>
2024-06-12 12:54:34 -0300

    Creating the order microservice

---
+++ services/order/src/main/java/com/ags/ecommerce/order/Order.java
@@ -0,0 +1,40 @@
+package com.ags.ecommerce.order;
+
+import com.ags.ecommerce.orderline.OrderLine;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "customer_order")
+public class Order {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String reference;
+    private BigDecimal totalAmount;
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+    private String customerId;
+    @OneToMany(mappedBy = "order")
+    private List<OrderLine> orderLines;
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDate createdAt;
+    @LastModifiedDate
+    @Column(insertable = false, nullable = false)
+    private LocalDate lastModifiedDate;
+}
